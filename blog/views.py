b8bb0aa639b35a0817f149215176bfef4a0a5200
Dead133 <Dead133@gmail.com>
2014-04-12 01:13:02 +0300

    Templates finished.

--- blog/views.py
+++ blog/views.py
@@ -72,6 +72,34 @@ def post_list(request):
                     'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, '
                     'and more recently with desktop publishing software like Aldus PageMaker including versions of '
                     'Lorem Ipsum.',
+            },
+        {
+            'title': 'Post title',
+            'thumbnail': 'img/pika_pika.jpg',
+            'link': '/installing-lamp-on-ubuntu-12.04/',
+            'date': '12 january 2014',
+            'comments_count': 15,
+            'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been '
+                    'the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley '
+                    'of type and scrambled it to make a type specimen book. It has survived not only five centuries, '
+                    'but also the leap into electronic typesetting, remaining essentially unchanged. It was '
+                    'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, '
+                    'and more recently with desktop publishing software like Aldus PageMaker including versions of '
+                    'Lorem Ipsum.',
+            },
+        {
+            'title': 'Post title',
+            'thumbnail': 'img/pika_pika.jpg',
+            'link': '/installing-lamp-on-ubuntu-12.04/',
+            'date': '12 january 2014',
+            'comments_count': 15,
+            'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been '
+                    'the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley '
+                    'of type and scrambled it to make a type specimen book. It has survived not only five centuries, '
+                    'but also the leap into electronic typesetting, remaining essentially unchanged. It was '
+                    'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, '
+                    'and more recently with desktop publishing software like Aldus PageMaker including versions of '
+                    'Lorem Ipsum.',
         },
         {
             'title': 'Post title',
